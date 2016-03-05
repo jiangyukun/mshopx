34bf8656550d12d3adea847cdd0479ace25ae11a
xuby <01jiangwei01@163.com>
2016-03-05 17:15:27 +0800

    增加spring  validator

---
+++ myshopx-service/src/main/java/com/gxkj/common/validate/Update.java
@@ -0,0 +1,7 @@
+package com.gxkj.common.validate;
+
+/**
+ * Created by dell on 2016/3/5.
+ */
+public interface Update {
+}
--- myshopx-service/src/main/java/com/gxkj/projects/myshopx/entitys/BaseModel.java
+++ myshopx-service/src/main/java/com/gxkj/projects/myshopx/entitys/BaseModel.java
@@ -2,10 +2,13 @@ package com.gxkj.projects.myshopx.entitys;
 
 
 
+import com.gxkj.common.validate.Add;
+import com.gxkj.common.validate.Update;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.Date;
@@ -20,14 +23,17 @@ public abstract class BaseModel implements   Serializable {
     @GenericGenerator(name="systemUUID",strategy="uuid")
     @GeneratedValue(generator="systemUUID")
     @Column(name = "id")
+    @NotNull(message = "{id.null}", groups = {Update.class})
     private String id;
 
 
     @Column(name="create_at",nullable = false)
     @Temporal(TemporalType.TIMESTAMP )
+    @NotNull(message = "{createdAt.null}", groups = {Add.class, Update.class})
     private Date createdAt;
 
     @Column(name="update_at",nullable = false)
+    @NotNull(message = "{updatedAt.null}", groups = {Add.class, Update.class})
     @Temporal(TemporalType.TIMESTAMP )
     private Date updatedAt;
 
